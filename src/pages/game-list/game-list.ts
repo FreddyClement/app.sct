78b6ef189a362bc6b3235821a1aea87444fad809
Freddy <freddy.clement10@gmail.com>
2018-09-26 23:29:33 +0200

    add game list and game details

---
+++ src/pages/game-list/game-list.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {GameDetailPage} from "../game-detail/game-detail";
+
+/**
+ * Generated class for the GameListPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-game-list',
+  templateUrl: 'game-list.html',
+})
+export class GameListPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    //faire l'appel en base de la catégorie choisis
+  }
+
+  gameDetail(){
+    this.navCtrl.push(GameDetailPage);
+  }
+
+}
